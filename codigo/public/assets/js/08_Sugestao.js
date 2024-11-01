b658be2ab2a00bb7c521fd23af6cc2b17e234959
Vitorfrios <vfrios@sga.pucminas.br>
2024-10-31 21:44:11 -0300

    moldagem da sidebar para as telas subtopicos

--- codigo/public/assets/js/08_Sugestao.js
+++ codigo/public/assets/js/08_Sugestao.js
@@ -1,14 +1,20 @@
-// ---------------------- SIDEBAR ---------------------- //
 
-// Marcar a aba ativa na sidebar
+/*
+COPIAR CODIGO NO TERMINAL PARA INICIALIZAR O JSON SERVER
+npx json-server --watch codigo/db/db.json --port 3000
+*/
+
+// ------------------- SIDE BAR CODIGO ------------------- //
+
 document.addEventListener("DOMContentLoaded", function() {
     function highlightActiveItem() {
-        const currentPage = window.location.pathname.split("/").pop(); // Pega o nome do arquivo da URL
+        const currentPage = window.location.pathname.split("/").pop(); 
 
-        const items = document.querySelectorAll('.sidebar ul li');
+        const items = document.querySelectorAll('.sidebar ul li, .sub-itens li');
         items.forEach(item => {
             item.classList.remove('active');
         });
+
         if (currentPage === '04_Dashboard.html') {
             document.getElementById('dashboard').classList.add('active');
         } else if (currentPage === '05_Cronograma_Diario.html') {
@@ -17,15 +23,28 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('tarefas').classList.add('active');
         } else if (currentPage === '07_Progresso.html') {
             document.getElementById('progresso').classList.add('active');
+        } else if (currentPage === '08_Sugestao.html') {
+            document.getElementById('sugestao').classList.add('active');
+        } else if (currentPage === '08.1_Ajuda_com_Horarios.html') {
+            document.getElementById('ajuda-horarios').classList.add('active');
+            document.getElementById('sugestao').classList.add('active'); 
+        } else if (currentPage === '08.2_Dicas_para_Estudo.html') {
+            document.getElementById('dicas-estudo').classList.add('active');
+            document.getElementById('sugestao').classList.add('active'); 
+        } else if (currentPage === '08.3_Dicas_para_Dormir.html') {
+            document.getElementById('dicas-dormir').classList.add('active');
+            document.getElementById('sugestao').classList.add('active'); 
+        } else if (currentPage === '08.4_Dicas_para_Desempenho.html') {
+            document.getElementById('dicas-desempenho').classList.add('active');
+            document.getElementById('sugestao').classList.add('active'); 
         } else if (currentPage === '09_Perfil.html') {
             document.getElementById('perfil').classList.add('active');
         } else if (currentPage === '10_Suporte_Feedback.html') {
             document.getElementById('feedback').classList.add('active');
-        } else if (currentPage === '08_Sugestao.html') {
-            document.getElementById('sugestao').classList.add('active');
         }
     }
 
-
     highlightActiveItem();
 });
+
+// ------------------- FIM DA SIDE BAR ------------------- //
